5524f287eaf0f61ea5fc0e51dcb821d443ed5093
elminster <kamase79@daum.net>
2020-10-21 21:29:15 +0900

    clang

--- layers/+lang/c-c++/packages.el
+++ layers/+lang/c-c++/packages.el
@@ -31,6 +31,9 @@
     ;; rtags
     (ivy-rtags :requires (ivy rtags))
     rtags
+    ; lsp mode
+    lsp-mode
+    lsp-ui
     ))
 
 (defun c-c++/init-cc-mode ()
@@ -164,3 +167,40 @@
     :post-init
     (dolist (mode '(c-mode c++-mode))
       (spacemacs/setup-helm-cscope mode))))
+
+(defun c-c++/init-lsp-mode ()
+  (use-package lsp-mode
+    :defer t
+    :config
+    (progn
+      ;(spacemacs/lsp-bind-keys)
+      (setq lsp-prefer-capf t)
+      (add-hook 'lsp-after-open-hook (lambda ()
+                                       "Setup xref jump handler and declare keybinding prefixes"
+                                       (spacemacs//setup-lsp-jump-handler)
+                                       (spacemacs//lsp-declare-prefixes-for-mode major-mode))))))
+
+(defun c-c++/init-lsp-ui ()
+  (use-package lsp-ui
+    :defer t
+    :config
+    :bind (:map lsp-ui-mode-map
+                ([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
+                ([remap xref-find-references] . lsp-ui-peek-find-references)
+                ("C-c u" . lsp-ui-imenu))
+    :hook (lsp-mode-hook . lsp-ui-mode)))
+
+
+;    (progn
+;      (if lsp-remap-xref-keybindings
+;          (progn (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+;                 (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)))
+;
+;      (spacemacs/lsp-define-key
+;       lsp-ui-peek-mode-map
+;       "h" #'lsp-ui-peek--select-prev-file
+;       "j" #'lsp-ui-peek--select-next
+;       "k" #'lsp-ui-peek--select-prev
+;       "l" #'lsp-ui-peek--select-next-file
+;       )
+;      )))
